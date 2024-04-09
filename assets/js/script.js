50f15f31edeae55783b000e832f4aae061eec9c1
Maria Salgado Salgado <maria.salgado@gmail.com>
2024-04-09 15:34:34 -0400

    trabajo desafio videos

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const miFuncion = (() => {
+    console.log("hace algo")
+})();
+
+
+const reproductor = (() => {
+    // Función privada para mostrar el video en el documento HTML
+    const mostrarVideo = (url, id) => {
+        const iframe = document.getElementById(id);
+        iframe.setAttribute('src', url);
+    };
+    // Función pública que realiza el llamado a la función privada para insertar los elementos
+    const playMultimedia = (url, id) => {
+        mostrarVideo(url, id);
+    };
+    // Retornar la función pública
+    return {
+        playMultimedia
+    };
+})();
+
